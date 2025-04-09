0e194f88433e93b84230d3ae3cef0daf27950cb7
DanilFlint <didenkod2010@gmail.com>
2025-04-09 21:39:42 +0300

    hw5 is done

--- hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,48 @@
 package ru.otus.hw.repositories;
 
+import lombok.Getter;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+@Getter
 @Repository
 public class JdbcAuthorRepository implements AuthorRepository {
 
+    private NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    private JdbcOperations jdbcOperations;
+
+    public JdbcAuthorRepository(NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        this.namedParameterJdbcOperations = namedParameterJdbcOperations;
+        this.jdbcOperations = namedParameterJdbcOperations.getJdbcOperations();
+    }
+
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        return jdbcOperations.query("select id, fullName from authors", new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+        var params = Map.of("id", id);
+        return Optional.ofNullable(namedParameterJdbcOperations
+                .queryForObject("select id, fullName from authors where id = :id", params, new AuthorRowMapper()));
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
-        public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+        public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Author(rs.getLong("id"), rs.getString("fullName"));
         }
     }
 }
--- hw05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -1,33 +1,49 @@
 package ru.otus.hw.repositories;
 
+import lombok.Getter;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+@Getter
 @Repository
 public class JdbcGenreRepository implements GenreRepository {
 
+    private NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    private JdbcOperations jdbcOperations;
+
+    public JdbcGenreRepository(NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        this.namedParameterJdbcOperations = namedParameterJdbcOperations;
+        this.jdbcOperations = namedParameterJdbcOperations.getJdbcOperations();
+    }
+
     @Override
     public List<Genre> findAll() {
-        return new ArrayList<>();
+        return jdbcOperations.query("select id, name from genres", new JdbcGenreRepository.GnreRowMapper());
     }
 
     @Override
     public Optional<Genre> findById(long id) {
-        return Optional.empty();
+        var sql = "select id, name from genres where id = :id";
+        var params = Map.of("id", id);
+        return Optional.ofNullable(namedParameterJdbcOperations
+                .queryForObject(sql, params, new JdbcGenreRepository.GnreRowMapper()));
     }
 
     private static class GnreRowMapper implements RowMapper<Genre> {
 
         @Override
         public Genre mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Genre(rs.getLong("id"), rs.getString("name"));
         }
     }
 }
